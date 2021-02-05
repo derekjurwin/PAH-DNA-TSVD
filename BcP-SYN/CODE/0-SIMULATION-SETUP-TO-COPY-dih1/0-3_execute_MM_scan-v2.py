945c31c6e20fb98f326dca67a14daa2fb7462210
derekjurwin <78337352+derekjurwin@users.noreply.github.com>
2021-02-04 17:46:39 -0800

    Delete BcP-SYN/CODE/0-SIMULATION-SETUP-TO-COPY-dih1 directory

--- BcP-SYN/CODE/0-SIMULATION-SETUP-TO-COPY-dih1/0-3_execute_MM_scan-v2.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-
-os.system('./6_MultiWrite_NAMD_MP2geom_OPT_conf_noBCs')
-
-time.sleep(5)
-
-os.system('./7_MultiExec_NAMD_OPTcalcs')
--- BcP-SYN/CODE/0-SIMULATION-SETUP-TO-COPY-dih1/0-5_run_malab_LS-v2.py
+++
@@ -1,5 +0,0 @@
-import os
-import time
-
-os.system('matlab -nodisplay -nodesktop -r "run z_new_mp2_new_mm_LS_UF1_loopFIX2.m"')
-
